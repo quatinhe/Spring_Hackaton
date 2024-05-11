1de072c2bd42f90dd685d134dac7fb92d7f684a4
Beatriz Santos <bsantos@opensoft.pt>
2024-05-11 10:23:23 +0100

    theme

---
+++ theme/theme.js
@@ -0,0 +1,52 @@
+
+export const Colors = {
+    primary: '#0068DC',
+    secondary: '#0095F0',
+    ternary: '#9737FF',
+    quartenary: '#EF83E2',
+    background: '#EDEDED',
+    text: '#3E3E3E',
+    accent: '#00DBDA',
+    border: '#CCCCCC',
+    error: '#DF0101',
+  };
+  
+
+  export const Fonts = {
+    regular: 'Jaldi-Regular',
+    bold: 'Jaldi-Bold',
+  };
+  
+  
+  export const FontSizes = {
+    small: 14,
+    regular: 16,
+    large: 18,
+  };
+  
+
+  export const Spacing = {
+    small: 8,
+    regular: 16,
+    large: 24,
+  };
+
+
+  export const BorderRadius = {
+    small: 4,
+    regular: 8,
+    large: 12,
+  };
+  
+
+  export const Shadow = {
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  };
+  
